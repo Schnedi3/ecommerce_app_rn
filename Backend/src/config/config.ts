9cc7383947eadf8c78ec42b90d49c0595024be4b
Schnedi3 <schnedi3@gmail.com>
2024-12-03 21:14:04 +0100

    Backend: Add clerk keys

--- Backend/src/config/config.ts
+++ Backend/src/config/config.ts
@@ -9,3 +9,6 @@ export const PG_HOST = process.env.PG_HOST;
 export const PG_PASSWORD = process.env.PG_PASSWORD;
 export const PG_PORT = process.env.PG_PORT;
 export const PG_USER = process.env.PG_USER;
+
+export const CLERK_PUBLISHABLE_KEY = process.env.CLERK_PUBLISHABLE_KEY;
+export const CLERK_SECRET_KEY = process.env.CLERK_SECRET_KEY;
