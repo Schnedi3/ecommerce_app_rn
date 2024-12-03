c725106759b9ad37645ed4ec9fb87c3c38ca97a5
Schnedi3 <schnedi3@gmail.com>
2024-12-04 00:30:07 +0100

    Frontend: Send token to backend

--- frontend/src/api/cart.ts
+++ frontend/src/api/cart.ts
@@ -1,15 +1,19 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { useUser } from "@clerk/clerk-expo";
+import { useAuth, useUser } from "@clerk/clerk-expo";
 
 import { customAxios } from "./customAxios";
 
 export const useGetCart = () => {
   const { user } = useUser();
+  const { getToken } = useAuth();
 
   return useQuery({
     queryKey: ["cart"],
     queryFn: async () => {
-      const { data } = await customAxios.get("/cart");
+      const token = await getToken();
+      const { data } = await customAxios.get("/cart", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
       return data;
     },
     initialData: [],
@@ -19,10 +23,18 @@ export const useGetCart = () => {
 
 export const useAddToCart = () => {
   const queryClient = useQueryClient();
+  const { getToken } = useAuth();
 
   return useMutation({
-    mutationFn: ({ id, quantity }: { id: number; quantity: number }) => {
-      return customAxios.post(`/cart/${id}`, { quantity });
+    mutationFn: async ({ id, quantity }: { id: number; quantity: number }) => {
+      const token = await getToken();
+      return customAxios.post(
+        `/cart/${id}`,
+        { quantity },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["cart"] });
@@ -32,10 +44,14 @@ export const useAddToCart = () => {
 
 export const useDeleteFromCart = () => {
   const queryClient = useQueryClient();
+  const { getToken } = useAuth();
 
   return useMutation({
-    mutationFn: (id: number) => {
-      return customAxios.delete(`/cart/${id}`);
+    mutationFn: async (id: number) => {
+      const token = await getToken();
+      return customAxios.delete(`/cart/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["cart"] });
